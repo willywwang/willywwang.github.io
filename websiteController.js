815fc065c2600a12b688b7ef1e0b23f6f32f55e2
Will Wang <wang.yw.william@gmail.com>
2017-02-12 16:34:54 -0800

    minor fix

--- websiteController.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('websiteApp', []);
-
-app.controller('WebsiteController', function($scope) {
-	$scope.showPopup = false;
-	$scope.openPopup = function() {
-		$scope.showPopup = !$scope.showPopup;
-	};
-});
